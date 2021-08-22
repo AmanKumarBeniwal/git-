2a7ea1518a0f7a169386b9b7ad16ac4aa295826f
AmanKumarBeniwal <techsolutions0007@gmail.com>
2021-08-22 21:07:41 +0530

    jhgj

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 // Configuring the database
 const mongoose = require('mongoose');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
